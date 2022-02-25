68f79846cebea2185ada7a95bb8bd51883883dc7
JWillumsen89 <jona95k7@stud.kea.dk>
2022-02-25 09:24:34 +0100

    added greet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
+
+    Scanner in = new Scanner(System.in);
+    String name;
+
+    void StartMessage() {
+      System.out.println("It works!");
+    }
+    void GreetUser(){
+    System.out.print("Please enter your name: ");
+    name = in.nextLine();
+      System.out.println("Welcome: " + name);
+  }
+
+  void go() {
+      StartMessage();
+      GreetUser();
+  }
+
   public static void main(String[] args) {
-    System.out.println("It works!");
+    new Main().go();
   }
 }
