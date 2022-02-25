13cea32e46328cd6aad05e628ad7baa027ec4bd6
JWillumsen89 <jona95k7@stud.kea.dk>
2022-02-25 09:30:23 +0100

    added askAboutAge

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,32 @@
+
 import java.util.Scanner;
 
 public class Main {
 
-    Scanner in = new Scanner(System.in);
-    String name;
+  Scanner in = new Scanner(System.in);
 
-    void StartMessage() {
-      System.out.println("It works!");
-    }
-    void GreetUser(){
+
+  void StartMessage() {
+    System.out.println("It works!");
+  }
+
+  void GreetUser() {
     System.out.print("Please enter your name: ");
-    name = in.nextLine();
-      System.out.println("Welcome: " + name);
+    String name = in.nextLine();
+    System.out.println("Hi: " + name);
+  }
+
+  void AskAboutAge() {
+    System.out.print("What is your age: ");
+    int age = in.nextInt();
+    System.out.println("You are: " + age + " years old.");
+
   }
 
   void go() {
-      StartMessage();
-      GreetUser();
+    StartMessage();
+    GreetUser();
+    AskAboutAge();
   }
 
   public static void main(String[] args) {
